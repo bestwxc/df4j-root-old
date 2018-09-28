053039f0b247010a7b329b7fce029104612bd443
wxc <wxc252@github.com>
2018-09-29 01:06:07 +0800

    增加注释

--- df-base/src/main/java/net/df/base/exception/DfException.java
+++ df-base/src/main/java/net/df/base/exception/DfException.java
@@ -4,22 +4,45 @@ package net.df.base.exception;
  * 自定义异常公共父类
  */
 public class DfException extends RuntimeException{
+    /**
+     * 构造函数
+     */
     public DfException() {
         super();
     }
 
+    /**
+     * 构造函数
+     * @param message
+     */
     public DfException(String message) {
         super(message);
     }
 
+    /**
+     * 构造函数
+     * @param message
+     * @param cause
+     */
     public DfException(String message, Throwable cause) {
         super(message, cause);
     }
 
+    /**
+     * 构造函数
+     * @param cause
+     */
     public DfException(Throwable cause) {
         super(cause);
     }
 
+    /**
+     * 构造函数
+     * @param message
+     * @param cause
+     * @param enableSuppression
+     * @param writableStackTrace
+     */
     protected DfException(String message, Throwable cause,
                                boolean enableSuppression,
                                boolean writableStackTrace) {
