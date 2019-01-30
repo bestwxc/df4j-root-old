c40d6dde65ef57c3c156cf5f6cb5165fb2f7cc22
王学成 <wxc252@hotmail.com>
2019-01-30 10:46:14 +0800

    ä¿优化AbstractRunnable

--- df4j-base/src/main/java/com/df4j/base/task/AbstractRunnable.java
+++ df4j-base/src/main/java/com/df4j/base/task/AbstractRunnable.java
@@ -10,27 +10,48 @@ public abstract class AbstractRunnable implements Runnable{
 
     private ThreadLocal<Long> beginTime = new ThreadLocal<>();
 
+    // 是否向外抛出异常，默认不抛，适用于大部分业务。部分调用该类处理逻辑希望抛出异常的，可以指定该参数为true
+    private boolean throwException = false;
+
     protected String getName(){
         return this.getClass().getName();
     }
 
+    public AbstractRunnable() {
+        this(false);
+    }
+
+    public AbstractRunnable(boolean throwException){
+        this.throwException = throwException;
+    }
+
+    public boolean isThrowException() {
+        return throwException;
+    }
+
+    public void setThrowException(boolean throwException) {
+        this.throwException = throwException;
+    }
+
     @Override
     public void run() {
+        String threadName = Thread.currentThread().getName();
         try {
             Long time = System.currentTimeMillis();
-            logger.info("Task-{} start.", this.getName());
+            logger.info("Task-{}[{}] start.", this.getName(), threadName);
             beginTime.set(time);
             this.run0();
-            logger.info("Task-{} end, cost {} ms",
-                    this.getName(), System.currentTimeMillis() - beginTime.get());
+            logger.info("Task-{}[{}] end, cost {} ms",
+                    this.getName(), threadName, System.currentTimeMillis() - beginTime.get());
             beginTime.remove();
         }catch (Throwable e){
-            String errorMsg = "Task-" + this.getName() + " error";
-            if(e instanceof Exception){
-                throw new DfException(errorMsg, e);
-            }else{
+            String errorMsg = "Task-" + this.getName() + "[" + threadName +"] error";
+            if(!throwException){
                 logger.error(errorMsg, e);
-                throw e;
+            }
+            if(throwException){
+                DfException exception = e instanceof DfException ? (DfException)e : new DfException(errorMsg, e);
+                throw exception;
             }
         }
     }
